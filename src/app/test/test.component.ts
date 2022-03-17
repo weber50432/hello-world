4a05190caeb22d1896b7f53839ecbb962a926375
weber <weber50432@gmail.com>
2022-03-17 23:36:02 +0800

    直接推送上github

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[app-test]',
+  template: `<div>Inline template</div>`,
+  styles: [
+    `
+      div {
+        color: red;
+      }
+    `,
+  ],
+})
+export class TestComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
